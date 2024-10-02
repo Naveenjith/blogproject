f234dbd333dc44b4db40b6b47e9e66514c467e60
Naveenjith <navinjith555@gmail.com>
2024-10-02 22:09:55 +0530

    time updated

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -72,7 +72,7 @@ MIDDLEWARE = [
 ]
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=20),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
